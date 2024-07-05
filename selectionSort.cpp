7979e771ecff2e973621fe56178ec82391082ad8
Grace Sharone T <145895890+grace073@users.noreply.github.com>
2024-07-05 23:30:05 +0530

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 void selectionSort(int *arr, int &n){
     for(int i=0;i<n-1;i++){
-        int min=i;
+        int mini=i;
         for(int j=i+1;j<n;j++){
-            if(arr[min]>arr[j]){ //descending order:"if(arr[min]<arr[j])"
-                min=j;            
+            if(arr[mini]>arr[j]){ //descending order:"if(arr[min]<arr[j])"
+                mini=j;            
             }
         }
         int temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;
+        arr[i]=arr[mini];
+        arr[mini]=temp;
     }
 }
 
